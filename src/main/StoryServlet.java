4e808709b96f151ab4570231f2f0fb13e7757b08
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-02-07 16:27:25 +0600

    Added authors names

--- src/main/StoryServlet.java
+++ src/main/StoryServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// Yerzhan Mademikhanov and Rustam Shumenov
+
 @WebServlet("/story")
 public class StoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
